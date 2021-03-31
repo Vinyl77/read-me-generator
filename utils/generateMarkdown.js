9bd8aad2bccb1ebd4bb8cbb721131ad912a7bb5d
Richard Yvarra <richard.yvarra@gmail.com>
2021-03-31 17:02:49 -0400

    test commit 5

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -26,7 +26,7 @@ function generateMarkdown(data) {
     console.log(data)
 
   return `# ${data.title},
-  ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)<br />
+  ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)<br />,
 
 
   
